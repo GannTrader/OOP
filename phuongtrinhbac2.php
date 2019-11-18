4aa7b009ff1e3c816d779d67f21a66a83bb0c731
= <=>
2019-11-18 11:32:26 +0700

    1

---
+++ phuongtrinhbac2.php
@@ -0,0 +1,48 @@
+<?php 
+
+class phuongtrinh{
+	public $a;
+	public $b;
+	public $c;
+	
+	public function phuongtrinh($a, $b, $c){
+		$this->a = $a;
+		$this->b = $b;
+		$this->c = $c;
+	}
+	public function getDelta(){
+		return ($this->b * $this->b) - 4 * $this->a * $this->c;
+	}
+	public function getResult(){
+		if(isset($_POST['ok'])){
+			if($this->getDelta()<0){
+				echo	"phương trình vô nghiệm";
+			} 
+			else if ($this->getDelta()==0){
+				echo	"Phương trình có nghiệm kép x = x1 = x2 = ". -$this->b/(2*$this->a);
+			} else {
+				echo	"phương trình có 2 nghiệm phân biệt"."<br>";
+				echo	"x1 = ".(-$this->b + sqrt($this->getDelta()))/(2*$this->a)."<br>";
+				echo	"x2 = ".(-$this->b - sqrt($this->getDelta()))/(2*$this->a)."<br>";
+			}
+		}
+	}
+}
+
+$pt1 = new phuongtrinh($_POST['a'], $_POST['b'], $_POST['c']);
+
+$pt1->getResult();
+
+?>
+
+<html>
+	<head></head>
+	<body>
+		<div>
+		<form action="#" method="post">
+			<input type="text" name="a" required>x<sup>2</sup> + <input type="text" name="b" required>x + <input type="text" name="c" required> = 0;
+			<input type="submit" name="ok" value="Result">
+		</form>
+		</div>
+	</body>
+</html>
